7eb845411236c39d810a5412cafc70f05fa67c8c
Daspudding7 <chatelain777@gmail.com>
2024-09-02 22:12:18 +0200

    basic prio queue

---
+++ PriorityQueue/IPQ.cs
@@ -0,0 +1,9 @@
+﻿namespace PriorityQueue
+{
+    internal interface IPQ<T>
+    {
+        public void Enqueue(T item, int prio);
+        public T Dequeue();
+        public T Peek();
+    }
+}
---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue<T> : IPQ<T>, IEnumerable<T>
+    {
+        private readonly List<(T item, int prio)> _queue = [];
+
+        public int Count { get { return _queue.Count; } }
+
+        public bool IsEmpty { get { return _queue.Count == 0; } }
+
+        public void Enqueue(T item, int prio)
+        {
+            var newElement = (item, prio);
+            int index = _queue.FindIndex(x => x.prio > prio);
+
+            if (index >= 0)
+            {
+                _queue.Insert(index, newElement);
+            }
+            else
+            {
+                _queue.Add(newElement);
+            }
+
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Dequeue attempt on empty queue.");
+            }
+
+            var (item, _) = _queue[0];
+            _queue.RemoveAt(0);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new InvalidOperationException("Peek was called but the priority queue is empty.");
+            }
+
+            var (item, _) = _queue[0];
+            return item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _queue[i].item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+           return GetEnumerator();
+        }
+    }
+}
